1402c15890c9b123c370208663a9f80ea6b659d4
DanielVega825 <danielvegalinares@hotmail.com>
2026-06-02 13:18:56 -0500

    config apibase

--- js/services/api.js
+++ js/services/api.js
@@ -4,7 +4,7 @@
  * Maneja autenticación (JWT Bearer), errores globales y respuestas
  */
 
-const API_BASE = 'http://localhost:8080/api/v1';
+const API_BASE = 'https://agrocinecta.onrender.com/api/v1';
 
 /**
  * Función central para hacer llamadas a la API
